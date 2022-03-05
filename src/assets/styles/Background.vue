2992c8085aa09b3c1953b1201d120e355989a110
hanankadry <hanan.kadry@icloud.com>
2022-03-05 21:05:11 +0200

    initial edits

---
+++ src/assets/styles/Background.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="shape" style="background-image: linear-gradient(to left, #0F121A, #374258); left: -55px; top: -100px; width: 250px;"/>
+  <div class="shape" style="border: 3px solid #F74464; left: -80px; top: -80px; width: 250px;"/>
+  <div class="shape" style="background-image: linear-gradient(to left, #416BC0, #374258); left: 25rem; top: -155px; width: 85px;"/>
+  <div class="shape" style="border: 3px solid #F74464; left: 25.6rem; top: -165px; width: 85px;"/>
+  <div class="shape" style="background-image: linear-gradient(to left, #374258, #416AAF); top: 25rem; left: 15rem; width: 80px;"/>
+  <div class="shape" style="border: 3px solid #F74464; top: 25.7rem; left: 14.3rem; width: 80px;"/>
+  <div class="shape" style="background-image: linear-gradient(to left, #7186AF, #374258); top: 20rem; left: 30rem; width: 200px;"/>
+  <div class="shape" style="border: 3px solid #F74464; top: 19.2rem; left: 31.5rem; width: 200px;"/>
+  <div class="box"/>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.shape {
+    height: 600px;
+    border-radius: 220px;
+    transform: rotate(-45deg);
+    position: fixed;
+    align-content: flex-start;
+}
+.box {
+    height: 100%;
+    width: 35%;
+    top: -1px;
+    left: 65%;
+    position: fixed;
+    border-radius: 95px 0 0 95px;
+    background-image: linear-gradient(#374258, #151D2E);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { BootstrapVue3, IconsPlugins } from 'bootstrap-vue-3'
 
-createApp(App).use(router).mount('#app')
+import '@popperjs/core';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+const app = createApp(App)
+
+app.use(router).use(BootstrapVue3).use(IconsPlugins).mount('#app')
