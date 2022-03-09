2989b8dfbbe5c93ce9f752567f3d8c6a4b00ebae
hanankadry <hanan.kadry@icloud.com>
2022-03-10 00:34:22 +0200

    adding firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Background from '@/components/Background'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBB8YSbamKhUF4D5cMhPlbrfZyJ3gkYdpA",
+  authDomain: "online-parking-d5cfb.firebaseapp.com",
+  projectId: "online-parking-d5cfb",
+  storageBucket: "online-parking-d5cfb.appspot.com",
+  messagingSenderId: "1097629580013",
+  appId: "1:1097629580013:web:b284f0ed49d99e55ec2ae4"
+};
 
 import '@popperjs/core'
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -10,5 +20,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 
+initializeApp(firebaseConfig);
 app.use(router).mount('#app')
 app.component("Background", Background)
