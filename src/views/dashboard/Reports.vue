bc801bf7725b9ae213c16c02b7f5952cdf35c55c
hanankadry <hanan.kadry@icloud.com>
2022-05-10 17:42:46 +0200

    fix reports

--- src/views/dashboard/Reports.vue
+++ src/views/dashboard/Reports.vue
@@ -4,42 +4,129 @@
     <breadcrumb :crumbLabel="label" :crumbHref="href" />
 
     <div class="container-fluid">
-      <div class="dropdown" >
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton1"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Find By
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li><a class="dropdown-item" href="#">Year</a></li>
-          <li><a class="dropdown-item" href="#">Month</a></li>
-          <li><a class="dropdown-item" href="#">Weekly</a></li>
-        </ul>
-      </div>
-      <div class="mb-3">
-        <label for="Input1" class="form-label">OR</label>
-        <label for="Input1" class="form-label">Find by Specific day</label>
-        <input type="date" class="form-control" id="Input1" />
-      </div>
-
-      <div class="col-lg">
+      <div class="container-fluid p-3">
         <div class="row">
-          <div class="col-xl" v-for="card in cards" :key="card.index" >
-            <statistics-card 
-              :cardIcon="card.icon"
-              :cardLabel="card.label"
-              :cardValue="card.value"
+          <div class="col-auto me-auto input-icons">
+            <i class="bi bi-search icon" />
+            <input
+              type="text"
+              class="m-1 input-md"
+              placeholder="Search"
+              v-model="searchInput"
             />
           </div>
-          <chart style="width:90%;"
-            :bar_old_data="bar.data"
-            :bar_new_data="bar.new_data"
-            :bar_name="bar.name"
-          />
+          <div class="col-auto dropdown">
+            <button
+              class="button-sm-fill dropdown-toggle"
+              type="button"
+              id="find-by"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Find By
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li><a class="dropdown-item" href="#">Year</a></li>
+              <li><a class="dropdown-item" href="#">Month</a></li>
+              <li><a class="dropdown-item" href="#">Weekly</a></li>
+              <li>
+                <a
+                  class="dropdown-item"
+                  href="#"
+                  data-bs-toggle="modal"
+                  data-bs-target=".custom-modal"
+                  >Custom</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+        <vue-good-table
+          :columns="columns"
+          :rows="rows"
+          styleClass="vgt-table"
+          class="table-style"
+          :pagination-options="{
+            enabled: true,
+            mode: 'remote',
+          }"
+          :search-options="{
+            enabled: false,
+            externalQuery: searchInput,
+          }"
+          :select-options="{
+            enabled: true,
+            selectOnCheckboxOnly: true,
+            selectionInfoClass: 'custom-class',
+            selectionText: 'rows selected',
+            clearSelectionText: 'clear',
+            disableSelectInfo: true,
+            selectAllByGroup: true,
+          }"
+        >
+        </vue-good-table>
+      </div>
+      <div class="data-container offset-9">
+        Total Number of Data: {{ rows.length }}
+      </div>
+
+      <div
+        class="modal fade custom-modal"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">Custom</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="row">
+                  <div class="col-md-6">
+                    <i class="bi bi-calendar2-week sm-icon" />
+                    <label for="from" class="form-label">From</label>
+                    <input
+                      type="date"
+                      v-model="date.from"
+                      class="form-control input-lg date"
+                      id="from"
+                      required
+                    />
+                  </div>
+                  <div class="col-md-6">
+                    <i class="bi bi-calendar2-week sm-icon" />
+                    <label for="to" class="form-label">To</label>
+                    <input
+                      type="date"
+                      v-model="date.to"
+                      class="form-control input-lg date"
+                      id="to"
+                      required
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="button-xs-unfill"
+                data-bs-dismiss="modal"
+              >
+                Cancel
+              </button>
+              <button type="button" class="button-xs-fill">Find</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -47,27 +134,50 @@
 </template>
 
 <script>
-import Chart from "@/components/Chart.vue";
-import StatisticsCard from "@/components/StatisticsCard.vue";
-
 export default {
-  components: {
-    chart: Chart,
-    "statistics-card": StatisticsCard,
-  },
   props: ["id"],
   data() {
     return {
       parking_id: this.id,
       label: "Reports",
       href: "/reports",
-
-      bar: {
-        data: [15, 11, 12, 42, 15, 21, 20],
-        new_data: [20],
-        name: "Registrations",
+      searchInput: "",
+      date: {
+        from: "",
+        to: "",
       },
-      cards: [],
+      rows: [],
+      columns: [
+        {
+          label: "Name",
+          field: "name",
+          sortable: false,
+        },
+        {
+          label: "Type",
+          field: "type",
+          sortable: false,
+        },
+        {
+          label: "Date",
+          field: "created_at",
+          sortable: false,
+          dateInputFormat: "yyyy-MM-dd",
+          dateOutputFormat: "MMM do yyyy",
+        },
+        {
+          label: "Status",
+          field: "status",
+          sortable: false,
+          width: "150px",
+        },
+        {
+          label: "Action",
+          field: "button",
+          sortable: false,
+          width: "200px",
+        },
+      ],
     };
   },
 };
@@ -84,21 +194,65 @@ export default {
 .dropdown {
   display: inline-block;
   position: relative;
-
 }
 .form-label {
-    margin-left: 10px;
-  color: #f74464;
+  margin-left: 10px;
+  color: #374258;
   font: 18pt;
   font-weight: bold;
   display: inline-block;
 }
 .form-control {
-    margin-left: 10px;
+  margin-left: 10px;
   display: inline-block;
   width: auto;
 }
+.icon {
+  padding-left: 20px;
+  padding-top: 10px;
+  font-size: 14pt;
+}
 
+.modal-content {
+  background-color: #374258;
+}
+
+.modal-content > .modal-header,
+.btn-close {
+  color: #f74464;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.modal-content > .modal-body {
+  background-color: white;
+}
+
+.input-lg {
+  padding-left: 2rem;
+  border-radius: 95px;
+  height: 50px;
+  background-color: #374258;
+  color: white;
+  border: none;
+}
+
+.sm-icon {
+  color: #f74464;
+  font-size: 1.5rem;
+  padding-right: 10px;
+}
+
+input[type="date"]::-webkit-calendar-picker-indicator {
+  background-image: url("@/assets/images/icons8-calendar-96.png");
+  background-position: end;
+  background-size: 20px 20px, 20px 20px;
+  background-repeat: no-repeat;
+}
+.data-container {
+  font-weight: bold;
+  font-size: 14pt;
+}
 </style>
 
 
--- src/views/pages/About.vue
+++ src/views/pages/About.vue
@@ -1,35 +1,73 @@
 <template>
-  <nav-bar :id="parking_id" />
+<!-- Start Header -->
+  <header class="header">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="container-fluid">
+        <router-link class="nav-brand" :to="{ path: `/` }">
+          <img src="@/assets/images/logo.jpg" class="logo" />
+        </router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target=".navbar-collapse"
+          aria-controls="navbar-collapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+        <div class="collapse navbar-collapse justify-content-end">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link
+                :to="{ path: `/` }"
+                class="nav-link"
+              >
+                <button class="button-sm-fill">Home</button>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <!-- End Header -->
+  <div class="container">
   <div class="card text-center">
     <div class="card-header">WHO ARE WE</div>
     <div class="card-body">
       <div class="card" style="width: 18rem">
         <div class="card-body">
-          <h5 class="card-title">Project Info</h5>
+          <h5 class="card-title">Team Info</h5>
           <p class="card-text">
-            It is a graduation project for Alexandria University - Faculty of
-            Science students.
+            Donia Hesham: 
+            <ul>
+              <li>Project Leader</li>
+              <li>Mobile Application Developer</li>
+            </ul>
+             Contact at: (+200) 123-456-7890
           </p>
         </div>
       </div>
-      <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Us</h5>
-    <p class="card-text">We are a Group of friends graduate at July 2022.
-      Implement graduation project 
-      Under supervision Dr. Islam El Kabani
-    </p>
-  </div>
-</div>
+      <div class="card" style="width: 18rem">
+        <div class="card-body">
+          <h5 class="card-title">Us</h5>
+          <p class="card-text">
+            We are a Group of friends graduate at July 2022. Implement
+            graduation project Under supervision Dr. Islam El Kabani
+          </p>
+        </div>
+      </div>
 
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Contact us</h5>
-    <p class="card-text">Contact Us at ..........@gmail.com</p>
-  </div>
-</div>
-
+      <div class="card" style="width: 18rem">
+        <div class="card-body">
+          <h5 class="card-title">Contact us</h5>
+          <p class="card-text">Contact Us at ..........@gmail.com</p>
+        </div>
+      </div>
     </div>
+  </div>
     <!-- Start Footer -->
     <footer class="footer my-0">
       <div class="container">
@@ -55,15 +93,25 @@
   </div>
 </template>
 <style scoped>
+.header {
+  background-color: #374258;
+}
+.logo {
+  border: 3px solid #f74464;
+}
+.nav-link {
+  cursor: pointer;
+}
+
 .footer {
   background-color: #374258;
   color: white;
   padding-top: 17px;
-  margin-top:auto;
-   position: fixed;
-   left: 0;
-   bottom: 0;
-   width: 100%;
+  margin-top: auto;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
 }
 .footer > a {
   text-decoration: none;
@@ -77,14 +125,13 @@
 .footer-icon:hover {
   color: #f74464;
 }
-.card{
-  display:inline;
+.card {
+  display: inline;
 }
-.card-header{
-  background-color: #374258;
-  color: white;
+.card-header {
+  font-size: 24pt;
+  color: #f74464;
+  text-transform: uppercase;
+  background-color: transparent;
 }
-
-
-
 </style>
